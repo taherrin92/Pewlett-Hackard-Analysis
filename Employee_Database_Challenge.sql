8f2d890dadc9b6639dda94ba91eacb3c964ec832
Tyson Herrin <taherrin92@gmail.com>
2021-12-18 19:13:13 -0600

    updated employee database

---
+++ Employee_Database_Challenge.sql
@@ -0,0 +1,47 @@
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM titles AS t
+INNER JOIN employees AS e
+ON (t.emp_no = e.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no,
+to_date DESC;
+
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+	ON(e.emp_no = de.emp_no)
+INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01'AND'1965-12-31')
+ORDER BY e.emp_no ASC;
